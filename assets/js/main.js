3b410e6351f511c15b7b640f43172566838e556b
Saranjith Palakkandy <saranvlmna@gmail.com>
2026-03-28 22:20:34 +0530

    ui change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,11 @@
-/**
- * Template Name: MyResume - v4.7.0
- * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function () {
   "use strict";
 
   const select = (el, all = false) => {
     el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
+    return all
+      ? [...document.querySelectorAll(el)]
+      : document.querySelector(el);
   };
 
   const on = (type, el, listener, all = false) => {
@@ -27,12 +19,8 @@
     }
   };
 
-  const onscroll = (el, listener) => {
-    el.addEventListener("scroll", listener);
-  };
-
   /**
-   * Navbar links active state on scroll
+   * Navbar active state on scroll
    */
   let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
@@ -52,10 +40,10 @@
     });
   };
   window.addEventListener("load", navbarlinksActive);
-  onscroll(document, navbarlinksActive);
+  document.addEventListener("scroll", navbarlinksActive);
 
   /**
-   * Scrolls to an element with header offset
+   * Smooth scroll to element
    */
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop;
@@ -68,14 +56,14 @@
   /**
    * Mobile nav toggle
    */
-  on("click", ".mobile-nav-toggle", function (e) {
+  on("click", ".mobile-nav-toggle", function () {
     select("body").classList.toggle("mobile-nav-active");
     this.classList.toggle("bi-list");
     this.classList.toggle("bi-x");
   });
 
   /**
-   * Scroll with offset on links with class .scrollto
+   * Scroll on .scrollto click
    */
   on(
     "click",
@@ -101,15 +89,13 @@
    * Page load initialization
    */
   window.addEventListener("load", () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
-      }
+    if (window.location.hash && select(window.location.hash)) {
+      scrollto(window.location.hash);
     }
 
     AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
+      duration: 800,
+      easing: "ease-out",
       once: true,
       mirror: false
     });
@@ -125,8 +111,8 @@
     new Typed(".typed", {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 80,
+      backSpeed: 40,
       backDelay: 2000
     });
   }
